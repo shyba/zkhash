89be9ae0683f68ee52c2dd34e95efd637f85fd9c
Victor Shyba <victor.shyba@gmail.com>
2025-06-22 18:32:18 -0300

    simplify guest

--- risc0-sha2+sha1/methods/guest/src/main.rs
+++ risc0-sha2+sha1/methods/guest/src/main.rs
@@ -3,20 +3,14 @@ use risc0_zkvm::guest::env;
 use sha2::Digest;
 use sha2::Sha256;
 use sha1::Sha1;
+use std::io::Read;
 
 fn main() {
-    let data: Vec<u8> = env::read();
+    let mut data = Vec::new();
+    let len = env::stdin().read_to_end(&mut data).unwrap() as u64;
     
-    let data_size = data.len() as u64;
-    env::commit_slice(&data_size.to_le_bytes());
+    env::commit_slice(&len.to_le_bytes());
 
-    let mut hasher = Sha1::new();
-    hasher.update(&data);
-    let sha1_hash: [u8; 20] = hasher.finalize().into();
-    env::commit_slice(&sha1_hash);
-    
-    let mut hasher = Sha256::new();
-    hasher.update(&data);
-    let sha256_hash: [u8; 32] = hasher.finalize().into();
-    env::commit_slice(&sha256_hash);
+    env::commit_slice(&Sha1::digest(&data));
+    env::commit_slice(&Sha256::digest(&data));
 }
